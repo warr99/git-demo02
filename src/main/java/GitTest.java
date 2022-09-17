f62b834485873f6576c6b154536951d1c0d399ca
warrior <warrior22@qq.com>
2022-09-17 14:19:48 +0800

    first commit in idea

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @author 陈希冉
+ * @version 1.0
+ * 演示
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,idea git");
+    }
+}
---
+++ src/main/java/GitTest02.java
@@ -0,0 +1,10 @@
+/**
+ * @author 陈希冉
+ * @version 1.0
+ * 演示
+ */
+public class GitTest02 {
+    public static void main(String[] args) {
+        System.out.println("hello,idea git again");
+    }
+}
